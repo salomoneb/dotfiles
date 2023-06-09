56f4d66fa2f9df7bb8dc7a82eb0b1d67f793ade5
Salomone Baquis <salomone.l.baquis@gmail.com>
2023-06-09 10:16:20 -0400

    update aliases

--- work/aliases.zsh
+++ work/aliases.zsh
@@ -1,5 +1,11 @@
 alias vscode="open -a Visual\ Studio\ Code.app"
 alias aliases="vscode /Users/salomonebaquis/.oh-my-zsh/custom/aliases.zsh"
-alias standup="open https://www.google.com/url\?q\=https://us02web.zoom.us/j/243002548\?pwd%3DajFSNHhWTkRGSmVodnpQajBacXhLQT09\&sa\=D\&source\=calendar\&ust\=1686085360964919\&usg\=AOvVaw0FeosHhhZJttVav6XsMCk6"
+alias standup="open 'https://us02web.zoom.us/j/243002548?pwd=ajFSNHhWTkRGSmVodnpQajBacXhLQT09'"
 alias slate-web="cd /Users/salomonebaquis/code/slate-web/"
 alias zshrc="vscode /Users/salomonebaquis/.zshrc"
+alias runslate="cd /Users/salomonebaquis/code/slate-web/ && make clear-public build start"
+alias clayconfig="cat /Users/salomonebaquis/.clayconfig"
+
+ci() {
+  clay export "$1" | sed "s/_components\/layout/_layouts\/layout/" | clay import --key local -p local.dev.slate.com
+}
