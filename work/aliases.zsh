14aaefffe33efd97634539e0b1f3b0e3882cf7da
Salomone Baquis <salomone.l.baquis@gmail.com>
2024-03-21 09:48:05 -0400

    updating aliases

--- work/aliases.zsh
+++ work/aliases.zsh
@@ -1,13 +1,46 @@
 alias vscode="open -a Visual\ Studio\ Code.app"
-alias aliases="vscode /Users/salomonebaquis/.oh-my-zsh/custom/aliases.zsh"
-alias standup="open 'https://us02web.zoom.us/j/243002548?pwd=ajFSNHhWTkRGSmVodnpQajBacXhLQT09'"
+
+alias_path=/Users/salomonebaquis/.oh-my-zsh/custom/aliases.zsh
+alias aliases="vscode '$alias_path'"
+alias aliaspath="echo '$alias_path'"
+
+alias standup="open 'https://meet.google.com/xmg-oetp-yag'"
+alias meet="echo 'xmg-oetp-yag' | pbcopy && echo 'copied xmg-oetp-yag to clipboard'"
 alias slate-web="cd /Users/salomonebaquis/code/slate-web/"
+alias myslate="cd /Users/salomonebaquis/code/my-slate/"
 alias zshrc="vscode /Users/salomonebaquis/.zshrc"
 alias runslate="cd /Users/salomonebaquis/code/slate-web/ && make clear-public build start"
 alias clayconfig="cat /Users/salomonebaquis/.clayconfig"
 alias nano="/opt/homebrew/bin/nano"
-alias check-elastic="lsof -i :9201"
+alias checkelastic="lsof -i :9201"
+alias prune='git branch --merged | egrep -v "(^\*|main|dev|stage)" | xargs git branch -d'
+alias python="/usr/bin/python3"
+alias branches="git branch"
+
+newbranch() {
+  if [ $# -eq 0 ]; then
+    >&2 echo "Pass in a branch name"
+    return
+  fi
+  git fetch && git checkout -b "$1" origin/stage
+}
 
 ci() {
   clay export "$1" | sed "s/_components\/layout/_layouts\/layout/" | clay import --key local -p local.dev.slate.com
+}
+
+login() {
+  cd /Users/salomonebaquis/code/slate-web/app/build && . env/bin/activate && fab "$1" log_in
+}
+
+boxlogin () {
+	aws ssm start-session --target "$( aws ec2 describe-instances --filter "Name=tag:Name,Values=$1" "Name=instance-state-name,Values=running" --query 'Reservations[*].Instances[*].[InstanceId,LaunchTime] [] | reverse(sort_by(@, &[1]))[0] | [0]' --output text )"
+}
+
+packages() {
+  cat package.json | jq '.dependencies * .devDependencies'
+}
+
+scripts() {
+  cat package.json | jq .scripts
 }
